f1e353fc1013c3531e8e3a785a9b651a44150913
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-04-05 00:16:32 +0300

    Assignment done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,17 @@
 import requests
-import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        # Return the content of the response as bytes
+        return response.content
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        # Decode bytes to string
+        response_body_str = response_body.decode('utf-8')
+        # Parse the string as JSON and return
+        return eval(response_body_str)
